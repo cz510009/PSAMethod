0dad2bff9489f351e861d4a08f03719180cbaf98
j.ikegami <cz510009@gmail.com>
2022-01-13 22:36:34 +0900

    タイトル設定

--- src/vue/vue.config.js
+++ src/vue/vue.config.js
@@ -5,5 +5,11 @@ module.exports = {
                 target: 'http://localhost:8080'
             }
         }
-    }
+    },
+    pages: {
+        index: {
+          entry: "src/main.js",
+          title: "TenZ式AIM感度調整 for VALORANT",
+        }
+      }
 };
