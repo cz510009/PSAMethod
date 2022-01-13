656aa3c726a5d04a1ac9b44c1ea5fa652157f10d
j.ikegami <cz510009@gmail.com>
2022-01-13 22:36:41 +0900

    API処理修正

--- src/main/java/com/cz510009/psamethod/Service/CalculateService.java
+++ src/main/java/com/cz510009/psamethod/Service/CalculateService.java
@@ -12,30 +12,27 @@ public class CalculateService {
     private double STANDARD_EDPI = 280.0;
 
     public SensitivityDto calculate(double dpi) {
-        System.out.println("start");
         double mid = round(STANDARD_EDPI / dpi);
         double high = round(mid * 1.5);
         double low = round(mid / 2);
         SensitivityDto dto = new SensitivityDto(high, mid, low);
-        System.out.println("end");
         return dto;
     }
 
     public SensitivityDto calculate(double sen, double mid) {
-        System.out.println("start");
-        double high = 0;
-        double low = 0;
-        if (sen >= mid) {
-            high = sen;
-            low = mid;
-            mid = round((sen + mid) / 2);
-        } else {
-            low = sen;
-            high = mid;
-            mid = round((sen + mid) / 2);
-        }
+        mid = sen + mid;
+        double high = mid * 1.5;
+        double low = mid * 0.5;
+        // if (sen >= mid) {
+        // high = sen;
+        // low = mid;
+        // mid = round((sen + mid) / 2);
+        // } else {
+        // low = sen;
+        // high = mid;
+        // mid = round((sen + mid) / 2);
+        // }
         SensitivityDto dto = new SensitivityDto(high, mid, low);
-        System.out.println("end");
         return dto;
     }
 
